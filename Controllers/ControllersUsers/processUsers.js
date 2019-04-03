af772f403fce9d4bd23005aa3987a4e40f11fae3
madou07ca <mdoubobobarry07ca@gmail.com>
2019-04-03 22:33:44 +0200

    Users

--- Controllers/ControllersUsers/processUsers.js
+++ Controllers/ControllersUsers/processUsers.js
@@ -5,20 +5,22 @@ var UsersModel  = require('../../Models/modelsUsers');
 
 
 module.exports = {
-	creerUser : (req , res) => {
+	creerUser : (res, req) => {
 	
 		//if(Object.keys(req.body).length === 7) {
 			//Attributs
-			const { nom, prenom, username, password} = req.body;
-			let user = new UsersModel({nom, prenom, username, password});
+            //const {nom, prenom, username, password} = req.body;
+			let user = new UsersModel({
+                nom : req.body.nom, 
+                prenom : req.body.prenom, 
+                username : req.body.username, 
+                password : req.body.password,
+            });
 			//enregistrer ds la BDD
 			user.save(err => {
 				//Si une erreur est survenue lors de la sauvegarde, envoyez-la. Sinon, envoyez un message de confirmation
 				err ? res.send(err) : res.json({message: 'Utilisateur ajouté!'});
 			});	
-		//} else{
-		//	res.json({error: "Vérifier les champs requis: sport, douleurAvant, douleurApres."});
-		//}
 		
 	},
 
